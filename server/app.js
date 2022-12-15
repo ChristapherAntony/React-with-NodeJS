20f25c9663e7a93255cd00703d07395359c2b2ee
Christapher Antony <christapher012@gmail.com>
2022-12-16 00:57:39 +0530

    admin login api done

--- server/app.js
+++ server/app.js
@@ -1,16 +1,15 @@
 const express =require('express')
 const app=express()
-const userRoutes=require('./routes/user')
-const adminRoutes=require('./routes/admin')
 const cors=require('cors')
 
-
-
+const userRoutes=require('./routes/user')
+const adminRoutes=require('./routes/admin')
 
 app.use(cors());
 app.use(express.json());
 
 
+//routes
 app.use('/api',userRoutes);
 app.use('/api/admin',adminRoutes);
 
