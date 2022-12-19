f0e2fff9c5d2c6d76a7dfc6589d460a7e2a33da1
Christapher Antony <christapher012@gmail.com>
2022-12-19 19:22:09 +0530

    last commit of review day

--- server/app.js
+++ server/app.js
@@ -8,14 +8,11 @@ app.use(express.static( 'public'));
 const userRoutes=require('./routes/user')
 const adminRoutes=require('./routes/admin')
 
-
-
 app.use(cors());
 app.use(express.json());
 
 
 //routes
-
 app.use('/api',userRoutes);
 app.use('/api/admin',adminRoutes);
 
